c2e60fc8861a4a7804de36c3e1ee42140ee83ecb
Benjamin Robbins <benro@Benjamins-MacBook-Air-2.local>
2026-03-10 11:01:31 -0600

    copy, drop

--- exercises/04_traits/12_copy/src/lib.rs
+++ exercises/04_traits/12_copy/src/lib.rs
@@ -1,6 +1,9 @@
 // TODO: implement the necessary traits to make the test compile and pass.
 //  You *can't* modify the test.
 
+use std::{ops::Add};
+
+#[derive(Debug, PartialEq, Copy, Clone)]
 pub struct WrappingU32 {
     value: u32,
 }
@@ -8,6 +11,14 @@ pub struct WrappingU32 {
 impl WrappingU32 {
     pub fn new(value: u32) -> Self {
         Self { value }
+    }
+}
+
+impl Add for WrappingU32 {
+    type Output = Self;
+    fn add(self, rhs: Self) -> Self::Output {
+        // same is Self::new, more explicit in my head
+        WrappingU32::new(self.value.wrapping_add(rhs.value))
     }
 }
 
@@ -20,6 +31,12 @@ mod tests {
         let x = WrappingU32::new(42);
         let y = WrappingU32::new(31);
         let z = WrappingU32::new(u32::MAX);
+        /* 
+            this is misleading because the error initially is that there is no add impl
+            you need to first impl add in general so you can sum the numbers but the point
+            is that y needs to be copy because y is used twice. it needs to handle the move
+        */
+
         assert_eq!(x + y + y + z, WrappingU32::new(103));
     }
 }
--- exercises/04_traits/13_drop/src/lib.rs
+++ exercises/04_traits/13_drop/src/lib.rs
@@ -2,6 +2,32 @@
 //  unless a certain operation has been performed on it.
 //  You can see the expected API in the tests below.
 
+struct DropBomb {
+    defused: bool
+}
+impl DropBomb {
+    pub fn new() -> Self {
+        DropBomb { defused: false }
+    }
+    pub fn defuse(&mut self) {
+        self.defused = true;
+    }
+
+}
+
+// basically the lesson here is that you must consider other held references, memory, or other conditions before dropping
+// custom drop implementations may be used
+impl Drop for DropBomb {
+    fn drop(&mut self) {
+        if !self.defused {
+            panic!("boom")
+        }
+        else {
+            println!("dropped normally");
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -9,10 +35,12 @@ mod tests {
     #[test]
     #[should_panic]
     fn test_drop_bomb() {
-        let bomb = DropBomb::new();
-        // The bomb should panic when dropped
+        let _bomb = DropBomb::new();
+        // The bomb should panic when dropped without 'clean up' method defuse()
     }
 
+    // after test is finished it'll still run the drop routine routine
+    // log message printed demonstrates that 'instances' will drop when going out of scope
     #[test]
     fn test_defused_drop_bomb() {
         let mut bomb = DropBomb::new();
